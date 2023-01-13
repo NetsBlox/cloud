9974f750d95ea7b49828aa6d1c0f7a730a2ab25b
Brian Broll <brian.broll@gmail.com>
2023-01-13 14:47:43 -0600

    Update set password page style

--- crates/cloud/src/users/html_template.rs
+++ crates/cloud/src/users/html_template.rs
@@ -3,32 +3,39 @@ pub(crate) fn set_password_page(username: &str) -> String {
 <html>
     <head>
         <link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/icon?family=Material+Icons\"/>
-        <link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css\"/>
+        <link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css\"/>
         <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"/>
         <title>Set NetsBlox Password</title>
     </head>
     <body>
-        <div style=\"height: 20%\"></div>
-        <div class=\"row\">
-            <div class=\"col s6 offset-s3\">
-                <p class=\"flow-text center-align\">Enter the new password for {username}:</p>
-            </div>
-        </div>
-        <div class=\"row\">
-            <div class=\"center-align\">
-                <form id=\"setPasswordForm\" method=\"post\">
-                    <label for=\"pwd\"></label><br/>
-                    <input type=\"password\" id=\"pwd\" required>
-                    <label for=\"confirmPwd\"></label><br/>
-                    <input type=\"password\" id=\"confirmPwd\" required>
-                </form>
-                <a id=\"setPasswordButton\" class=\"waves-effect waves-light btn\">Change Password</a>
-                <p id=\"errorMsg\" class=\"errorMsg\"></p>
-                <p id=\"successMsg\" class=\"successMsg\"></p>
+
+        <div class=\"blue-grey lighten-5 valign-wrapper\" style=\"height:100%\">
+            <div class=\"container\">
+                <div class=\"row\">
+                    <div class=\"col s12 m8 offset-m2 l6 offset-l3\">
+                        <div class=\"card\">
+                            <div class=\"card-content\">
+                                <span class=\"card-title\">Enter a new password for {username}:</span>
+
+                                <div class=\"row\">
+                                    <label for=\"pwd\"></label><br/>
+                                    <input type=\"password\" id=\"pwd\" required>
+                                    <label for=\"confirmPwd\"></label><br/>
+                                    <input type=\"password\" id=\"confirmPwd\" required>
+                                </div>
+                                <div class=\"row\">
+                                    <a id=\"setPasswordButton\" class=\"waves-effect waves-light btn\">Change Password</a>
+                                    <p id=\"errorMsg\" class=\"errorMsg\"></p>
+                                    <p id=\"successMsg\" class=\"successMsg\"></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 
-        <script src=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js\"></script>
+        <script src=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js\"></script>
         <script>
             const queryString = location.href.split('?').pop();
             const allowButton = document.getElementById('setPasswordButton');
@@ -53,12 +60,14 @@ pub(crate) fn set_password_page(username: &str) -> String {
                     body: JSON.stringify(password)
                 }};
                 const response = await fetch(url, opts);
+                const hasToken = url.includes('token');
                 if (response.status === 200) {{
                     successMsg.innerHTML = 'Password updated!';
+                }} else if (response.status === 403 && hasToken) {{
+                    errorMsg.innerHTML = 'Unable to set password: Invalid reset token.';
                 }} else {{
                     errorMsg.innerHTML = 'Unable to set password: ' + await response.text();
                 }}
-                console.log(response.status);
             }};
         </script>
         <style>
