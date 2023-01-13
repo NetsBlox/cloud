5a65628a311aa709b8be12b7e0d9d97ade0a739e
Brian Broll <brian.broll@gmail.com>
2023-01-13 14:11:57 -0600

    Clean up set password email

--- crates/cloud/src/users/html_template.rs
+++ crates/cloud/src/users/html_template.rs
@@ -1,36 +1,86 @@
-use lettre::message::MultiPart;
+pub(crate) fn set_password_page(username: &str) -> String {
+    format!("
+<html>
+    <head>
+        <link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/icon?family=Material+Icons\"/>
+        <link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css\"/>
+        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"/>
+        <title>Set NetsBlox Password</title>
+    </head>
+    <body>
+        <div style=\"height: 20%\"></div>
+        <div class=\"row\">
+            <div class=\"col s6 offset-s3\">
+                <p class=\"flow-text center-align\">Enter the new password for {username}:</p>
+            </div>
+        </div>
+        <div class=\"row\">
+            <div class=\"center-align\">
+                <form id=\"setPasswordForm\" method=\"post\">
+                    <label for=\"pwd\"></label><br/>
+                    <input type=\"password\" id=\"pwd\" required>
+                    <label for=\"confirmPwd\"></label><br/>
+                    <input type=\"password\" id=\"confirmPwd\" required>
+                </form>
+                <a id=\"setPasswordButton\" class=\"waves-effect waves-light btn\">Change Password</a>
+                <p id=\"errorMsg\" class=\"errorMsg\"></p>
+                <p id=\"successMsg\" class=\"successMsg\"></p>
+            </div>
+        </div>
 
-pub(crate) fn set_password_email(username: &str, url: &str) -> MultiPart {
-    let html =    format!(
-        "<h1>Password Reset Request<h1>
-        <p>
-            Click the link below to reset the password for {username}. If you did not request a password reset, this email can be ignored.
-            <br/>
-            <br/>
-            <a href=\"{url}\">{url}</a>
-            <br/>
-            <br/>
-            Cheers,<br/>
-            the NetsBlox team
-        </p>
-        ",
-        username = username,
-        url = url
-    );
-    let txt = format!(
-        "Password Reset Request
-        
-        Click the link below to reset the password for {username}. If you did not request a password reset, this email can be ignored.
+        <script src=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js\"></script>
+        <script>
+            const queryString = location.href.split('?').pop();
+            const allowButton = document.getElementById('setPasswordButton');
+            const errorMsg = document.getElementById('errorMsg');
+            const successMsg = document.getElementById('successMsg');
+            allowButton.onclick = async function() {{
+                // ensure the passwords match
+                const password = document.getElementById('pwd').value;
+                const passwordConfirm = document.getElementById('confirmPwd');
+                if (password !== passwordConfirm.value) {{
+                    return passwordConfirm.setCustomValidity('Passwords do not match');
+                }}
+                passwordConfirm.setCustomValidity('');
+                errorMsg.innerHTML = '';
 
+                const url = location.href;
+                const opts = {{
+                    method: 'PATCH',
+                    headers: {{
+                        'Content-Type': 'application/json',
+                    }},
+                    body: JSON.stringify(password)
+                }};
+                const response = await fetch(url, opts);
+                if (response.status === 200) {{
+                    successMsg.innerHTML = 'Password updated!';
+                }} else {{
+                    errorMsg.innerHTML = 'Unable to set password: ' + await response.text();
+                }}
+                console.log(response.status);
+            }};
+        </script>
+        <style>
+            .successMsg {{
+                color: blue;
+            }}
 
-        {url}
+            .errorMsg {{
+                color: red;
+            }}
 
+            .username {{
+                font-size: 1.5em;
+                text-align: center;
+            }}
 
-        Cheers,
-        the NetsBlox team",
-        username = username,
-        url = url
-        );
-
-    MultiPart::alternative_plain_html(txt, html)
+            @media only screen and (min-width: 750px){{
+                .scope {{
+                    font-size: 1.25em;
+                }}
+            }}
+        </style>
+<html/>
+    ", username=username)
 }
