51cab331008583c7f3b4d372355f5e94e8640214
Brian Broll <brian.broll@gmail.com>
2023-01-31 16:52:22 -0600

    Block messages to group members from unknown users

--- crates/cloud/src/network/topology/network.rs
+++ crates/cloud/src/network/topology/network.rs
@@ -8,6 +8,7 @@ use lazy_static::lazy_static;
 use log::warn;
 use lru::LruCache;
 use mongodb::bson::{doc, DateTime};
+use netsblox_cloud_common::SentMessage;
 use serde_json::json;
 use std::collections::{HashMap, HashSet};
 use std::str::FromStr;
@@ -16,7 +17,7 @@ use std::time::{Duration, SystemTime};
 
 use crate::app_data::AppData;
 use crate::common::api::{ProjectId, SaveState};
-use crate::common::{ProjectMetadata, SentMessage};
+use crate::common::ProjectMetadata;
 use crate::errors::InternalError;
 use crate::network::topology::address::ClientAddress;
 
@@ -286,21 +287,11 @@ impl Topology {
         .into_iter()
         .flatten();
 
-<<<<<<< Updated upstream
-        // TODO: if the recipient is in a group, ensure the sender is a friend
-        recipients.clone().for_each(|client| {
-            client.addr.do_send(message.clone()).unwrap();
-        });
-
-        // maybe record the message
-=======
->>>>>>> Stashed changes
         if let Some(app) = &self.app_data {
             // check if the message is allowed
-            // TODO: get user membership
-            // TODO: get user membership
-            let recipients = self.valid_recipients(&app, &msg.sender, recipients).await;
-            // TODO: make method for getting all the cached users?
+            let recipients = self
+                .allowed_recipients(app, &msg.sender, recipients.collect())
+                .await;
 
             recipients.iter().for_each(|client| {
                 client.addr.do_send(message.clone()).unwrap();
@@ -362,17 +353,59 @@ impl Topology {
         }
     }
 
-    async fn valid_recipients(
+    /// Get the allowed recipients of a message. If the recipient is a
+    /// member of a group, ensure that the sender can message that group.
+    async fn allowed_recipients<'a>(
         &self,
         app: &AppData,
         sender: &ClientId,
-        recipients: impl Iterator<Item = &Client>,
-    ) -> Vec<&Client> {
-        let usernames = recipients.map(|rcp| self.usernames.get(&rcp.id));
+        recipients: Vec<&'a Client>,
+    ) -> Vec<&'a Client> {
         let sender = self.usernames.get(sender);
-        // TODO: check if the recipients are group members
-        // TODO: if so, check that the sender is a friend
-        todo!()
+
+        // TODO: for each recipient,
+        //   - if he/she is a member, allow if the sender is a friend
+        //   - else, allow
+        let recipient_names: HashSet<_> = recipients
+            .iter()
+            .filter_map(|rcp| self.usernames.get(&rcp.id))
+            .cloned()
+            .collect();
+        let members = app
+            // TODO: remove dupes from recipient list
+            .keep_members(recipient_names)
+            .await
+            .unwrap_or_default();
+
+        let deny_list: HashSet<_> = if let Some(sender) = sender {
+            if app.is_admin(sender).await {
+                // allow messages from admins
+                std::iter::empty().collect()
+            } else {
+                // message only allowed from group member/owner
+                join_all(members.into_iter().map(|member| async {
+                    let friends = app.get_friends(&member).await.unwrap_or_default();
+                    (member, friends.contains(sender))
+                }))
+                .await
+                .into_iter()
+                .filter_map(|(rec_name, is_friend)| if is_friend { Some(rec_name) } else { None })
+                .collect()
+            }
+        } else {
+            // messages to any group member will be blocked
+            members.into_iter().collect()
+        };
+
+        recipients
+            .into_iter()
+            .filter(|rec| {
+                self.usernames
+                    .get(&rec.id)
+                    .map(|username| !deny_list.contains(username))
+                    .unwrap_or(true)
+            })
+            .collect()
     }
 
     fn has_client(&self, id: &ClientId) -> bool {
