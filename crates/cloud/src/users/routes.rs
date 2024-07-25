2918085bc83f49948daf0d05f899927f1092e03a
Saman Kittani <87401990+samankittani@users.noreply.github.com>
2024-07-25 05:26:17 -0500

    add unlink test (#251)
    * add unlink test
    
    * fixed test
    
    ---------
    
    Co-authored-by: Brian Broll <brian.broll@gmail.com>

--- crates/cloud/src/users/routes.rs
+++ crates/cloud/src/users/routes.rs
@@ -1203,4 +1203,45 @@ mod tests {
     //     async fn test_link_account_duplicate() {
     //         todo!();
     //     }
+
+    // NOTE: add linkedaccounts to NewUser
+    #[actix_web::test]
+    async fn test_unlink_account() {
+        let linked_account = api::LinkedAccount {
+            username: "user".to_string(),
+            strategy: "snap".to_string(),
+        };
+        let mut user: User = api::NewUser {
+            username: "user".into(),
+            email: "user@netsblox.org".into(),
+            password: None,
+            group_id: None,
+            role: None,
+        }
+        .into();
+        user.linked_accounts.push(linked_account.clone());
+
+        test_utils::setup()
+            .with_users(&[user.clone()])
+            .run(|app_data| async move {
+                let app = test::init_service(
+                    App::new()
+                        .wrap(test_utils::cookie::middleware())
+                        .app_data(web::Data::new(app_data.clone()))
+                        .configure(config),
+                )
+                .await;
+
+                let req = test::TestRequest::post()
+                    .uri(&format!("/{}/unlink", &user.username))
+                    .cookie(test_utils::cookie::new(&user.username))
+                    .set_json(&linked_account)
+                    .to_request();
+
+                let user: api::User = test::call_and_read_body_json(&app, req).await;
+                dbg!(&user);
+                assert!(user.linked_accounts.is_empty());
+            })
+            .await;
+    }
 }
